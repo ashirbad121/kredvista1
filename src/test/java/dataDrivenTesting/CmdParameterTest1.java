4041eb09a0569349c021e9569fafbc262418395d
ashirbad121 <ashirbad.sonu123@gmail.com>
2023-11-30 13:32:14 +0530

    commit

--- src/test/java/dataDrivenTesting/CmdParameterTest1.java
+++ src/test/java/dataDrivenTesting/CmdParameterTest1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class CmdParameterTest {
+public class CmdParameterTest1 {
 
 	@Test
 	public void cmdParametertest1()
